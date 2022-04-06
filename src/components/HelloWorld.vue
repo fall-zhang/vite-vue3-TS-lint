3496c0c7a28f66eeb0fbbece34c72b03b08c3e33
张博飞 <bofei@zhongyukeji.ltd>
2022-04-06 17:47:19 +0800

    添加 prettier eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
+// import  {Button } from ''
+// defineProps<{ msg: string }>()
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1>Todo List</h1>
+  <h1>{{ count }}</h1>
+  <ElButton />
+  <p>开始开发一个 todoList</p>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+const app = createApp(App)
+app.mount('#app')
+app.use(ElementPlus,{size:'small',zIndex:3000})
