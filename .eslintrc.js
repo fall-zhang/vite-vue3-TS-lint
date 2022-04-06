b307a3487223fadd0de4f66b4be9e1892ec1362e
张博飞 <bofei@zhongyukeji.ltd>
2022-04-06 18:00:42 +0800

    修改eslint配置信息

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,30 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:vue/essential", "standard", "prettier"],
+  extends: ['plugin:vue/essential', 'standard', 'prettier'],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    "vue/no-multiple-template-root": 0,
+    'vue/no-multiple-template-root': 0,
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 10,
+        multiline: 1,
+      },
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: [2, 'never'],
   },
-};
+}
