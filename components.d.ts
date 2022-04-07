f6cfaed3ec8f60b0e82404a371436a779b97ff05
张博飞 <bofei@zhongyukeji.ltd>
2022-04-07 10:43:49 +0800

    yarn modify no commit-msg

--- components.d.ts
+++ components.d.ts
@@ -2,10 +2,10 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module "vue" {
+declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import("element-plus/es")["ElButton"];
+    ElButton: typeof import('element-plus/es')['ElButton'];
   }
 }
 
-export {};
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,4 +16,4 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-});
+})
