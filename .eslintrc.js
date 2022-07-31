49bcd3481ae831b46dc16868add578b45bef021e
DESKTOP-T6MD988\19852 <a90172451@gmail.com>
2022-07-31 19:48:53 +0800

    📝文档(Vue):

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,39 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
+  // global: {
+  //   // vue 的全局变量
+  //   ref: true,
+  //   reactive: true,
+  // },
   extends: ['plugin:vue/vue3-strongly-recommended', 'standard', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
+    'vue/first-attribute-linebreak': 0,
     'vue/no-multiple-template-root': 0,
     'vue/max-attributes-per-line': [
       2,
       {
         singleline: 5,
-        multiline: 1,
-      },
+        multiline: 4
+      }
     ],
     quotes: [
       2,
       'single',
       {
         avoidEscape: true,
-        allowTemplateLiterals: true,
-      },
+        allowTemplateLiterals: true
+      }
     ],
     semi: [2, 'never'],
-    'no-irregular-whitespace': 2,
-  },
+    'no-irregular-whitespace': 2
+  }
 }
