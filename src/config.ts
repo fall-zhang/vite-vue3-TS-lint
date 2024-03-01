23ea59e0a3fc9178e5475dc360a706b86e606c0a
Fall <zhangbofei@163.com>
2024-03-01 11:44:38 +0800

    tool(global): 🔧 更新全局配置和请求封装

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-const env = import.meta.env
-export const isDevelopment = env.MODE === 'development'
-
-export default {
-  defaultRouter: '/home',
-  baseUrl: '/api',
-  env,
-  development: isDevelopment
-}
--- src/global/global.d.ts
+++ src/global/global.d.ts
@@ -1,4 +1,3 @@
-
 declare module 'mitt'{
 
   export = HTMLElement
