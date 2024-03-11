a27685b8ebc9c2c1af569f91eb007b32bff86fec
fall-zhang <zhangbofei@163.com>
2024-03-11 14:21:46 +0800

    feat(component): 🚀 新增一个组件

---
+++ src/components/form/inline-form/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<script lang="ts">
+export default {
+  name: 'InlineForm'
+}
+</script>
+<style lang="scss" scoped></style>
