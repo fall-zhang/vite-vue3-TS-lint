18e6c40fb80bac3136e072aef8764c160724a3ba
fall-zhang <zhangbofei@163.com>
2024-06-24 10:25:34 +0800

    feat(component): 🚀 添加一个可以直接渲染的 echarts box 组件

---
+++ src/components/data-display/ChartBox.vue
@@ -0,0 +1,19 @@
+<!-- 一个只需要传入 option 就可以完整渲染父元素 100% 空间的 echarts 组件 -->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  // 传递 option 直接渲染 echarts
+  option: {
+
+  },
+  // data: [],
+  // xAxis: []
+})
+</script>
+
+<style lang="scss" scoped></style>
