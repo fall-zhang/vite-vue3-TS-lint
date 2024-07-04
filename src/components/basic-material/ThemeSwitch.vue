0f390d2f117a9d2cdb05882d114f2a4ca22eac45
fall-zhang <zhangbofei@163.com>
2024-07-04 10:02:59 +0800

    添加文本输入特效，主题切换按钮

---
+++ src/components/basic-material/ThemeSwitch.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-switch v-model="globalStore.isDark" inline-prompt :active-icon="SunOne" :inactive-icon="Moon" @change="switchDark" />
+</template>
+
+<script setup lang="ts" name="SwitchDark">
+import { useTheme } from "@/hooks/useTheme";
+import { useGlobalStore } from "@/stores/modules/global";
+import { SunOne, Moon } from "@icon-park/vue-next";
+
+const { switchDark } = useTheme();
+const globalStore = useGlobalStore();
+</script>
